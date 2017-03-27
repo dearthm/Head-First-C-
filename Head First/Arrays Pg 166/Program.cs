1b669be6217e1a6c1bc64ba69a44b7f48b0ca480
dearthm <dearthml@gmail.com>
2017-03-27 12:38:06 -0400

    Added arrays

---
+++ Head First/Arrays Pg 166/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_Pg_166
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[] myArray = new bool[15];
+            myArray[4] = true;
+            myArray[11] = true;
+            myArray.ToList().Sort();
+
+            foreach (bool value in myArray)
+            {
+                Console.WriteLine("myArray Element 4= " + value);
+            }
+            
+            Console.Read();
+
+        }
+    }
+}
