ef517a9ec8fe70705bcccddc5290f4e6d97a978f
dearthm <dearthml@gmail.com>
2017-03-27 11:45:55 -0400

    Added properties to form elephant

--- Head First/WindowsFormsApplication1/Form1.Designer.cs
+++ Head First/WindowsFormsApplication1/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApplication1
 {
-    partial class Form1
+    partial class frmElephant
     {
         /// <summary>
         /// Required designer variable.
@@ -28,57 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnLloyd = new System.Windows.Forms.Button();
+            this.btnLucinda = new System.Windows.Forms.Button();
+            this.btnSwap = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnLloyd
             // 
-            this.button1.Location = new System.Drawing.Point(99, 66);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnLloyd.Location = new System.Drawing.Point(99, 66);
+            this.btnLloyd.Name = "btnLloyd";
+            this.btnLloyd.Size = new System.Drawing.Size(75, 23);
+            this.btnLloyd.TabIndex = 0;
+            this.btnLloyd.Text = "Lloyd";
+            this.btnLloyd.UseVisualStyleBackColor = true;
+            this.btnLloyd.Click += new System.EventHandler(this.btnLloyd_Click);
             // 
-            // button2
+            // btnLucinda
             // 
-            this.button2.Location = new System.Drawing.Point(99, 108);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnLucinda.Location = new System.Drawing.Point(99, 108);
+            this.btnLucinda.Name = "btnLucinda";
+            this.btnLucinda.Size = new System.Drawing.Size(75, 23);
+            this.btnLucinda.TabIndex = 1;
+            this.btnLucinda.Text = "Lucinda";
+            this.btnLucinda.UseVisualStyleBackColor = true;
+            this.btnLucinda.Click += new System.EventHandler(this.btnLucinda_Click);
             // 
-            // button3
+            // btnSwap
             // 
-            this.button3.Location = new System.Drawing.Point(99, 150);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnSwap.Location = new System.Drawing.Point(99, 150);
+            this.btnSwap.Name = "btnSwap";
+            this.btnSwap.Size = new System.Drawing.Size(75, 23);
+            this.btnSwap.TabIndex = 2;
+            this.btnSwap.Text = "Swap";
+            this.btnSwap.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // frmElephant
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.btnSwap);
+            this.Controls.Add(this.btnLucinda);
+            this.Controls.Add(this.btnLloyd);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmElephant";
+            this.Text = "Elephant";
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnLloyd;
+        private System.Windows.Forms.Button btnLucinda;
+        private System.Windows.Forms.Button btnSwap;
     }
 }
 
--- Head First/WindowsFormsApplication1/Form1.cs
+++ Head First/WindowsFormsApplication1/Form1.cs
@@ -10,11 +10,41 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
-    public partial class Form1 : Form
+    public partial class frmElephant : Form
     {
-        public Form1()
+
+        Elephant lLoyd;
+        Elephant lucinda;
+        public frmElephant()
         {
             InitializeComponent();
+
+            lucinda = new Elephant() { Name = "Lucinda", EarSize = 33 };
+            lLoyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
+
         }
+
+        private void btnLloyd_Click(object sender, EventArgs e)
+        {
+            lLoyd.WhoAmI();
+        }
+
+        private void btnLucinda_Click(object sender, EventArgs e)
+        {
+            lucinda.WhoAmI();
+        }
+    }
+
+    public class Elephant
+    {
+
+        public int EarSize;
+        public string Name;
+
+        public void WhoAmI()
+        {
+            MessageBox.Show("My ears are " + EarSize + " inches long.","My name is " + Name + " ...");
+        }
+
     }
 }
